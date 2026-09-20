58919112262286a15ead1e3b9ae54315de75eadf
Engin Polat <polatengin97@gmail.com>
2026-09-20 18:21:35 +1000

    I added abstract Transaction class

---
+++ PersonalBudgetTracker/Transaction.cs
@@ -0,0 +1,20 @@
+﻿namespace PersonalBudgetTracker
+{
+    public abstract class Transaction
+    {
+        public decimal Amount { get; set; }
+        public string Category { get; set; }
+        public string Description { get; set; }
+        public DateTime Date { get; set; }
+
+        public Transaction(decimal amount, string category, string description, DateTime date)
+        {
+            Amount = amount;
+            Category = category;
+            Description = description;
+            Date = date;
+        }
+
+        public abstract decimal GetValue();
+    }
+}
