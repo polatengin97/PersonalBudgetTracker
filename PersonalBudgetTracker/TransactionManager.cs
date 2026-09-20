eeed908dba5090bd7e126785b104228d8e4233e5
Engin Polat <polatengin97@gmail.com>
2026-09-20 18:34:00 +1000

    TransactionManager class added fo calculation

---
+++ PersonalBudgetTracker/TransactionManager.cs
@@ -0,0 +1,36 @@
+﻿namespace PersonalBudgetTracker
+{
+    public class TransactionManager
+    {
+        public List<Transaction> Transactions { get; private set; }
+
+        public TransactionManager()
+        {
+            Transactions = new List<Transaction>();
+        }
+
+        public void AddTransaction(Transaction transaction)
+        {
+            Transactions.Add(transaction);
+        }
+
+        public decimal GetTotalIncome()
+        {
+            return Transactions
+                .Where(transaction => transaction is Income)
+                .Sum(transaction => transaction.Amount);
+        }
+
+        public decimal GetTotalExpenses()
+        {
+            return Transactions
+                .Where(transaction => transaction is Expense)
+                .Sum(transaction => transaction.Amount);
+        }
+
+        public decimal GetBalance()
+        {
+            return Transactions.Sum(transaction => transaction.GetValue());
+        }
+    }
+}
