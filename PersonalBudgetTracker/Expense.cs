6fd570c6ee4ad01c6aeeeff773f2fea54bb392dc
Engin Polat <polatengin97@gmail.com>
2026-09-20 18:30:24 +1000

    Expense Class added

---
+++ PersonalBudgetTracker/Expense.cs
@@ -0,0 +1,15 @@
+﻿namespace PersonalBudgetTracker
+{
+    public class Expense : Transaction
+    {
+        public Expense(decimal amount, string category, string description, DateTime date)
+            : base(amount, category, description, date)
+        {
+        }
+
+        public override decimal GetValue()
+        {
+            return -Amount;
+        }
+    }
+}
