167aada93370e635971646fbadb7857cfb9d3d75
Engin Polat <polatengin97@gmail.com>
2026-09-20 18:24:03 +1000

    income class added

---
+++ PersonalBudgetTracker/Income.cs
@@ -0,0 +1,15 @@
+﻿namespace PersonalBudgetTracker
+{
+    public class Income : Transaction
+    {
+        public Income(decimal amount, string category, string description, DateTime date)
+            : base(amount, category, description, date)
+        {
+        }
+
+        public override decimal GetValue()
+        {
+            return Amount;
+        }
+    }
+}
